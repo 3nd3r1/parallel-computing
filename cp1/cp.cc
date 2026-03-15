d621611cd39701132e466b62a6fcf178fbdcb160
3nd3r1 <ender.spam.guru@gmail.com>
2026-03-15 16:19:35 +0200

    ex(cp1): normalize the matrix

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,3 +1,6 @@
+#include <cmath>
+#include <vector>
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -6,4 +9,22 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float *data, float *result) {}
+void correlate(int ny, int nx, const float *data, float *result) {
+    std::vector<std::vector<double>> matrix(ny, std::vector<double>(nx, 0.0));
+    for (int y = 0; y < ny; y++) {
+        double avg = 0;
+        double mag = 0;
+        for (int x = 0; x < nx; x++) {
+            matrix[y][x] = data[x + y * nx];
+            avg += matrix[y][x];
+            mag += matrix[y][x] * matrix[y][x];
+        }
+        avg /= nx;
+        mag = std::sqrt(mag);
+
+        for (int x = 0; x < nx; x++) {
+            matrix[y][x] -= avg;
+            matrix[y][x] /= mag;
+        }
+    }
+}
