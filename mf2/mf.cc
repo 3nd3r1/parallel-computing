0ab021014975f5c47d5714165c6273553f1f06dd
3nd3r1 <ender.spam.guru@gmail.com>
2026-03-18 08:53:15 +0200

    ex(mf2): add the naive approach

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -1,3 +1,11 @@
+#include <algorithm>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+using std::vector, std::max, std::min;
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -7,4 +15,28 @@ This is the function you need to implement. Quick reference:
   max(x-hx, 0) <= a < min(x+hx+1, nx), max(y-hy, 0) <= b < min(y+hy+1, ny)
   in out[x + y*nx].
 */
-void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {}
+#pragma omp parallel for
+void mf(int ny, int nx, int hy, int hx, const float *in, float *out) {
+    for (int y = 0; y < ny; y++) {
+        for (int x = 0; x < nx; x++) {
+            vector<float> vals;
+            vals.reserve(2 * hx * 2 * hy);
+
+            for (int i = max(0, y - hy); i < min(ny, y + hy + 1); i++) {
+                for (int j = max(0, x - hx); j < min(nx, x + hx + 1); j++) {
+                    vals.push_back(in[j + i * nx]);
+                }
+            }
+
+            std::nth_element(vals.begin(), vals.begin() + vals.size() / 2,
+                             vals.end());
+            if (vals.size() % 2 == 0) {
+                float a = *std::max_element(vals.begin(),
+                                            vals.begin() + vals.size() / 2);
+                out[x + y * nx] = (a + vals[vals.size() / 2]) / 2;
+            } else {
+                out[x + y * nx] = vals[vals.size() / 2];
+            }
+        }
+    }
+}
