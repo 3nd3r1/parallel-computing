c975016d8fc0eb833194c5de7f2d304208971062
3nd3r1 <ender.spam.guru@gmail.com>
2026-03-20 15:11:20 +0200

    ex(cp2a): Make the matrix 1d

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -1,6 +1,8 @@
 #include <cmath>
 #include <vector>
 
+using std::vector;
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -10,23 +12,19 @@ This is the function you need to implement. Quick reference:
 - only parts with 0 <= j <= i < ny need to be filled
 */
 void correlate(int ny, int nx, const float *data, float *result) {
-    std::vector<std::vector<double>> matrix(ny, std::vector<double>(nx, 0.0));
+    vector<double> matrix(nx * ny, 0.0);
+
     for (int y = 0; y < ny; y++) {
         double sum = 0;
-        for (int x = 0; x < nx; x++) {
-            sum += data[x + y * nx];
-        }
-
         double sum_sq = 0;
         for (int x = 0; x < nx; x++) {
+            sum += data[x + y * nx];
             sum_sq += (double)data[x + y * nx] * (double)data[x + y * nx];
         }
-
         double avg = sum / nx;
         double mag = std::sqrt(sum_sq - nx * avg * avg);
-
         for (int x = 0; x < nx; x++) {
-            matrix[y][x] = (data[x + y * nx] - avg) / mag;
+            matrix[x + y * nx] = (data[x + y * nx] - avg) / mag;
         }
     }
 
@@ -34,7 +32,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
         for (int i = j; i < ny; i++) {
             double val = 0;
             for (int k = 0; k < nx; k++) {
-                val += matrix[i][k] * matrix[j][k];
+                val += matrix[k + i * nx] * matrix[k + j * nx];
             }
             result[i + j * ny] = (float)val;
         }
