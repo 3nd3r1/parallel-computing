0764c8e9c380508baa2f1ff0ff77cc16317ca88e
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-07 14:06:14 +0300

    ex(cp3a): she schedule static with chunk of 1

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -61,7 +61,7 @@ void correlate(int ny, int nx, const float *data, float *result) {
         }
     }
 
-#pragma omp parallel for schedule(guided)
+#pragma omp parallel for schedule(static, 1)
     for (int i = 0; i < n_blocks; i++) {
         for (int j = i; j < n_blocks; j++) {
             double4_t val[block_size][block_size] = {};
