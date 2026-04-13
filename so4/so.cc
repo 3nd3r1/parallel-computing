5bbf9926c424878f1fe35f7fa00a964325b2ee95
3nd3r1 <ender.spam.guru@gmail.com>
2026-04-13 16:26:07 +0300

    ex(so4): use a preallocated tmp buffer

--- so4/so.cc
+++ so4/so.cc
@@ -1,48 +1,34 @@
 #include <algorithm>
+#include <cstring>
 #include <omp.h>
-#include <vector>
-
-using std::vector;
 
 typedef unsigned long long data_t;
 
-void merge(data_t *data, int l, int m, int r) {
+void merge(data_t *data, data_t *tmp, int l, int m, int r) {
     int nl = m - l + 1;
     int nr = r - m;
 
-    vector<data_t> L(nl), R(nr);
+    std::memcpy(tmp + l, data + l, nl * sizeof(data_t));
+    std::memcpy(tmp + m + 1, data + m + 1, nr * sizeof(data_t));
 
-    for (int i = 0; i < nl; i++)
-        L[i] = data[l + i];
-    for (int j = 0; j < nr; j++)
-        R[j] = data[m + 1 + j];
-
-    int i = 0, j = 0;
+    int i = l, j = m + 1;
     int k = l;
-    while (i < nl && j < nr) {
-        if (L[i] <= R[j]) {
-            data[k] = L[i];
-            i++;
+    while (i <= m && j <= r) {
+        if (tmp[i] <= tmp[j]) {
+            data[k++] = tmp[i++];
         } else {
-            data[k] = R[j];
-            j++;
+            data[k++] = tmp[j++];
         }
-        k++;
     }
 
-    while (i < nl) {
-        data[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < nr) {
-        data[k] = R[j];
-        j++;
-        k++;
-    }
+    while (i <= m)
+        data[k++] = tmp[i++];
+
+    while (j <= r)
+        data[k++] = tmp[j++];
 }
 
-void mergeSort(data_t *data, int l, int r) {
+void mergeSort(data_t *data, data_t *tmp, int l, int r) {
     if (r - l < 1000000) {
         std::sort(data + l, data + r + 1);
         return;
@@ -50,17 +36,17 @@ void mergeSort(data_t *data, int l, int r) {
 
     int m = l + (r - l) / 2;
 #pragma omp task
-    mergeSort(data, l, m);
+    mergeSort(data, tmp, l, m);
 #pragma omp task
-    mergeSort(data, m + 1, r);
+    mergeSort(data, tmp, m + 1, r);
 #pragma omp taskwait
-    merge(data, l, m, r);
+    merge(data, tmp, l, m, r);
 }
 
 void psort(int n, data_t *data) {
+    data_t *tmp = new data_t[n];
 #pragma omp parallel
 #pragma omp single
-    {
-        mergeSort(data, 0, n - 1);
-    }
+    mergeSort(data, tmp, 0, n - 1);
+    delete[] tmp;
 }
