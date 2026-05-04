cf7c42fa2ee7f4193009fba3fe2a26086e95c912
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-04 14:10:03 +0300

    ex(cp5): do normalization with cuda

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -14,20 +14,42 @@ static inline void check(cudaError_t err, const char *context) {
 
 static inline int divup(int a, int b) { return (a + b - 1) / b; }
 
-static inline int roundup(int a, int b) { return divup(a, b) * b; }
+// static inline int roundup(int a, int b) { return divup(a, b) * b; }
 
 #define CHECK(x) check(x, #x)
 
-__global__ void mykernel(float *result, double *matrix, int nx, int ny) {
+__global__ void normalize_kernel(float *matrix, int nx, int ny) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= ny)
+        return;
+
+    float sum = 0;
+    for (int x = 0; x < nx; x++) {
+        sum += matrix[y * nx + x];
+    }
+    float avg = sum / nx;
+
+    float sum_sq = 0;
+    for (int x = 0; x < nx; x++) {
+        matrix[y * nx + x] -= avg;
+        sum_sq += matrix[y * nx + x] * matrix[y * nx + x];
+    }
+    float mag = sqrt(sum_sq);
+    for (int x = 0; x < nx; x++) {
+        matrix[y * nx + x] /= mag;
+    }
+}
+
+__global__ void correlate_kernel(float *result, float *matrix, int nx, int ny) {
     int i = threadIdx.x + blockIdx.x * blockDim.x;
     int j = threadIdx.y + blockIdx.y * blockDim.y;
     if (i >= ny || j >= ny || i < j)
         return;
-    double val = 0;
+    float val = 0;
     for (int k = 0; k < nx; k++) {
         val += matrix[i * nx + k] * matrix[j * nx + k];
     }
-    result[i + j * ny] = (float)val;
+    result[i + j * ny] = val;
 }
 
 /*
@@ -38,40 +60,24 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
+
 void correlate(int ny, int nx, const float *data, float *result) {
-    double *matrix = new double[ny * nx];
+    float *matrixGPU = NULL;
+    CHECK(cudaMalloc((void **)&matrixGPU, ny * nx * sizeof(float)));
+    CHECK(cudaMemcpy(matrixGPU, data, ny * nx * sizeof(float),
+                     cudaMemcpyHostToDevice));
 
-    for (int y = 0; y < ny; y++) {
-        double avg = 0;
-        for (int x = 0; x < nx; x++) {
-            matrix[y * nx + x] = data[x + y * nx];
-            avg += matrix[y * nx + x];
-        }
-        avg /= nx;
-        double mag = 0;
-        for (int x = 0; x < nx; x++) {
-            matrix[y * nx + x] -= avg;
-            mag += matrix[y * nx + x] * matrix[y * nx + x];
-        }
-        mag = std::sqrt(mag);
-        for (int x = 0; x < nx; x++) {
-            matrix[y * nx + x] /= mag;
-        }
-    }
+    normalize_kernel<<<divup(ny, 256), 256>>>(matrixGPU, nx, ny);
 
-    double *matrixGPU = NULL;
-    CHECK(cudaMalloc((void **)&matrixGPU, ny * nx * sizeof(double)));
     float *resultGPU = NULL;
     CHECK(cudaMalloc((void **)&resultGPU, ny * ny * sizeof(float)));
     CHECK(cudaMemset(resultGPU, 0, ny * ny * sizeof(float)));
-    CHECK(cudaMemcpy(matrixGPU, matrix, ny * nx * sizeof(double),
-                     cudaMemcpyHostToDevice));
 
     dim3 dimBlock(16, 16);
     dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
-    mykernel<<<dimGrid, dimBlock>>>(resultGPU, matrixGPU, nx, ny);
-    CHECK(cudaGetLastError());
+    correlate_kernel<<<dimGrid, dimBlock>>>(resultGPU, matrixGPU, nx, ny);
 
+    CHECK(cudaGetLastError());
     CHECK(cudaMemcpy(result, resultGPU, ny * ny * sizeof(float),
                      cudaMemcpyDeviceToHost));
     CHECK(cudaFree(matrixGPU));
