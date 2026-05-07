13c4fcae418d4c4d6a45ecf77218723c5e7904c4
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-07 17:08:52 +0300

    ex(cp5): reuse data

--- cp5/cp.cu
+++ cp5/cp.cu
@@ -14,7 +14,7 @@ static inline void check(cudaError_t err, const char *context) {
 
 static inline int divup(int a, int b) { return (a + b - 1) / b; }
 
-// static inline int roundup(int a, int b) { return divup(a, b) * b; }
+static inline int roundup(int a, int b) { return divup(a, b) * b; }
 
 #define CHECK(x) check(x, #x)
 
@@ -40,30 +40,58 @@ __global__ void normalize_kernel(float *matrix, int nx, int ny) {
     }
 }
 
-__global__ void transpose_kernel(float *matrix, float *matrix_t, int nx,
-                                 int ny) {
-    int y = blockIdx.x * blockDim.x + threadIdx.x;
-    if (y >= ny)
-        return;
+__global__ void pad_transpose_kernel(const float *data, float *matrix, int nx,
+                                     int ny, int padded_nx, int padded_ny) {
+    int ja = threadIdx.x;
+    int i = blockIdx.y;
 
-    for (int x = 0; x < nx; x++) {
-        matrix_t[x * ny + y] = matrix[y * nx + x];
+    for (int jb = 0; jb < padded_nx; jb += 64) {
+        int j = ja + jb;
+        float val = (i < ny && j < nx) ? data[i * nx + j] : 0;
+        matrix[j * padded_ny + i] = val;
     }
 }
 
-__global__ void correlate_kernel(float *result, float *matrix, float *matrix_t,
-                                 int nx, int ny) {
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
-    int j = threadIdx.y + blockIdx.y * blockDim.y;
+__global__ void correlate_kernel(float *result, float *matrix, int nx, int ny,
+                                 int padded_nx, int padded_ny) {
+    int ia = threadIdx.x;
+    int ja = threadIdx.y;
+    int ic = blockIdx.x;
+    int jc = blockIdx.y;
 
-    if (i >= ny || j < i || j >= ny)
-        return;
-
-    float val = 0;
-    for (int k = 0; k < nx; k++) {
-        val += matrix_t[k * ny + i] * matrix[j * nx + k];
+    float val[8][8];
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            val[i][j] = 0;
+        }
     }
-    result[i * ny + j] = val;
+
+    for (int k = 0; k < nx; k++) {
+        float x[8];
+        float y[8];
+        for (int ib = 0; ib < 8; ib++) {
+            int i = ic * 64 + ib * 8 + ia;
+            x[ib] = matrix[padded_ny * k + i];
+        }
+        for (int jb = 0; jb < 8; jb++) {
+            int j = jc * 64 + jb * 8 + ja;
+            y[jb] = matrix[padded_ny * k + j];
+        }
+        for (int ib = 0; ib < 8; ib++) {
+            for (int jb = 0; jb < 8; jb++) {
+                val[ib][jb] += x[ib] * y[jb];
+            }
+        }
+    }
+    for (int ib = 0; ib < 8; ++ib) {
+        for (int jb = 0; jb < 8; ++jb) {
+            int i = ic * 64 + ib * 8 + ia;
+            int j = jc * 64 + jb * 8 + ja;
+            if (i < ny && j < ny) {
+                result[ny * i + j] = val[ib][jb];
+            }
+        }
+    }
 }
 
 /*
@@ -76,32 +104,48 @@ This is the function you need to implement. Quick reference:
 */
 
 void correlate(int ny, int nx, const float *data, float *result) {
-    float *matrixGPU = NULL;
-    CHECK(cudaMalloc((void **)&matrixGPU, ny * nx * sizeof(float)));
-    CHECK(cudaMemcpy(matrixGPU, data, ny * nx * sizeof(float),
+    int padded_ny = roundup(ny, 64);
+    int padded_nx = roundup(nx, 64);
+
+    float *dataGPU = NULL;
+    CHECK(cudaMalloc((void **)&dataGPU, ny * nx * sizeof(float)));
+    CHECK(cudaMemcpy(dataGPU, data, ny * nx * sizeof(float),
                      cudaMemcpyHostToDevice));
 
-    normalize_kernel<<<divup(ny, 256), 256>>>(matrixGPU, nx, ny);
-
-    float *matrixTGPU = NULL;
-    CHECK(cudaMalloc((void **)&matrixTGPU, nx * ny * sizeof(float)));
-    CHECK(cudaMemset(matrixTGPU, 0, ny * nx * sizeof(float)));
-
-    transpose_kernel<<<divup(ny, 256), 256>>>(matrixGPU, matrixTGPU, nx, ny);
+    float *matrixGPU = NULL;
+    CHECK(
+        cudaMalloc((void **)&matrixGPU, padded_ny * padded_nx * sizeof(float)));
+    CHECK(cudaMemset(matrixGPU, 0, padded_ny * padded_nx * sizeof(float)));
 
     float *resultGPU = NULL;
     CHECK(cudaMalloc((void **)&resultGPU, ny * ny * sizeof(float)));
     CHECK(cudaMemset(resultGPU, 0, ny * ny * sizeof(float)));
 
-    dim3 dimBlock(16, 16);
-    dim3 dimGrid(divup(ny, dimBlock.x), divup(ny, dimBlock.y));
-    correlate_kernel<<<dimGrid, dimBlock>>>(resultGPU, matrixGPU, matrixTGPU,
-                                            nx, ny);
+    {
+        normalize_kernel<<<divup(ny, 256), 256>>>(dataGPU, nx, ny);
+        CHECK(cudaGetLastError());
+    }
 
-    CHECK(cudaGetLastError());
+    {
+        dim3 dimBlock(64, 1);
+        dim3 dimGrid(1, padded_ny);
+        pad_transpose_kernel<<<dimGrid, dimBlock>>>(dataGPU, matrixGPU, nx, ny,
+                                                    padded_nx, padded_ny);
+        CHECK(cudaGetLastError());
+    }
+
+    {
+        dim3 dimBlock(8, 8);
+        dim3 dimGrid(padded_ny / 64, padded_ny / 64);
+        correlate_kernel<<<dimGrid, dimBlock>>>(resultGPU, matrixGPU, nx, ny,
+                                                padded_nx, padded_ny);
+        CHECK(cudaGetLastError());
+    }
+
     CHECK(cudaMemcpy(result, resultGPU, ny * ny * sizeof(float),
                      cudaMemcpyDeviceToHost));
+
+    CHECK(cudaFree(dataGPU));
     CHECK(cudaFree(matrixGPU));
-    CHECK(cudaFree(matrixTGPU));
     CHECK(cudaFree(resultGPU));
 }
