e01d5c2dda2a843c19fb93bab817294e6186f208
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-08 15:32:12 +0300

    ex(is6a): move the allocation outside

--- is6a/is.cc
+++ is6a/is.cc
@@ -40,67 +40,72 @@ Result segment(int ny, int nx, const float *data) {
 
     int total_s = pref_s[nx + nxp * ny];
 
-#pragma omp parallel for schedule(dynamic, 1)
-    for (int y0 = 0; y0 < ny; y0++) {
-        float min_tsse = 1e9;
-        Result result = Result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
+#pragma omp parallel
+    {
         vector<int> diff_s(nxp);
         vector<float> inv_in(nxp);
         vector<float> inv_out(nxp);
-        for (int y1 = y0 + 1; y1 <= ny; y1++) {
-            int iy0 = nxp * y0;
-            int iy1 = nxp * y1;
-            for (int x = 0; x <= nx; x++) {
-                diff_s[x] = pref_s[x + iy1] - pref_s[x + iy0];
-            }
-            float row_n = (y1 - y0);
-            float total_nd = (nx * ny);
-            for (int k = 1; k <= nx; k++) {
-                float in_n = row_n * k;
-                float out_n = total_nd - in_n;
-                inv_in[k] = 1.0 / in_n;
-                inv_out[k] = (out_n > 0.0) ? 1.0 / out_n : 0.0;
-            }
-            for (int x0 = 0; x0 < nx; x0++) {
-                float d_x0 = diff_s[x0];
-                for (int x1 = x0 + 1; x1 <= nx; x1++) {
-                    int k = x1 - x0;
-                    float inv_i = inv_in[k];
-                    float inv_o = inv_out[k];
-                    if (inv_o == 0.0)
-                        continue;
+#pragma omp for schedule(dynamic, 1)
+        for (int y0 = 0; y0 < ny; y0++) {
+            float min_tsse = 1e9;
+            Result result = Result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
+            for (int y1 = y0 + 1; y1 <= ny; y1++) {
+                int iy0 = nxp * y0;
+                int iy1 = nxp * y1;
+                for (int x = 0; x <= nx; x++) {
+                    diff_s[x] = pref_s[x + iy1] - pref_s[x + iy0];
+                }
+                float row_n = (y1 - y0);
+                float total_nd = (nx * ny);
+                for (int k = 1; k <= nx; k++) {
+                    float in_n = row_n * k;
+                    float out_n = total_nd - in_n;
+                    inv_in[k] = 1.0 / in_n;
+                    inv_out[k] = (out_n > 0.0) ? 1.0 / out_n : 0.0;
+                }
+                for (int x0 = 0; x0 < nx; x0++) {
+                    float d_x0 = diff_s[x0];
+                    for (int x1 = x0 + 1; x1 <= nx; x1++) {
+                        int k = x1 - x0;
+                        float inv_i = inv_in[k];
+                        float inv_o = inv_out[k];
+                        if (inv_o == 0.0)
+                            continue;
 
-                    float inside_sum = diff_s[x1] - d_x0;
-                    float outside_sum = total_s - inside_sum;
+                        float inside_sum = diff_s[x1] - d_x0;
+                        float outside_sum = total_s - inside_sum;
 
-                    float inside_sse = inside_sum * (1 - inside_sum * inv_i);
-                    float outside_sse = outside_sum * (1 - outside_sum * inv_o);
+                        float inside_sse =
+                            inside_sum * (1 - inside_sum * inv_i);
+                        float outside_sse =
+                            outside_sum * (1 - outside_sum * inv_o);
 
-                    float tsse = inside_sse + outside_sse;
+                        float tsse = inside_sse + outside_sse;
 
-                    if (tsse > global_min_tsse)
-                        continue;
+                        if (tsse > global_min_tsse)
+                            continue;
 
-                    if (tsse < min_tsse) {
-                        min_tsse = tsse;
-                        result = Result{
-                            y0,
-                            x0,
-                            y1,
-                            x1,
-                            {(outside_sum * inv_o), (outside_sum * inv_o),
-                             (outside_sum * inv_o)},
-                            {(inside_sum * inv_i), (inside_sum * inv_i),
-                             (inside_sum * inv_i)}};
+                        if (tsse < min_tsse) {
+                            min_tsse = tsse;
+                            result = Result{
+                                y0,
+                                x0,
+                                y1,
+                                x1,
+                                {(outside_sum * inv_o), (outside_sum * inv_o),
+                                 (outside_sum * inv_o)},
+                                {(inside_sum * inv_i), (inside_sum * inv_i),
+                                 (inside_sum * inv_i)}};
+                        }
                     }
                 }
             }
-        }
 #pragma omp critical
-        {
-            if (min_tsse < global_min_tsse) {
-                global_min_tsse = min_tsse;
-                global_result = result;
+            {
+                if (min_tsse < global_min_tsse) {
+                    global_min_tsse = min_tsse;
+                    global_result = result;
+                }
             }
         }
     }
