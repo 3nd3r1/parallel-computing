d0dc0354a40e10a19c2936a15a96e1905d9b9b55
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-08 14:01:52 +0300

    ex(so5): add a depth limit

--- so5/so.cc
+++ so5/so.cc
@@ -27,18 +27,23 @@ int partition(data_t *data, int l, int r) {
     return i + 1;
 }
 
-void quickSort(data_t *data, int l, int r) {
-    if (r - l < 1000000) {
+void quickSort(data_t *data, int l, int r, int depth = 0) {
+    if (r - l < 10000 || depth > 40) {
         std::sort(data + l, data + r + 1);
         return;
     }
 
     int pi = partition(data, l, r);
+    if (depth < 8) {
 #pragma omp task
-    quickSort(data, l, pi - 1);
+        quickSort(data, l, pi - 1, depth + 1);
 #pragma omp task
-    quickSort(data, pi + 1, r);
+        quickSort(data, pi + 1, r, depth + 1);
 #pragma omp taskwait
+    } else {
+        quickSort(data, l, pi - 1, depth + 1);
+        quickSort(data, pi + 1, r, depth + 1);
+    }
 }
 
 void psort(int n, data_t *data) {
