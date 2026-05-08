4977a0627ba9aef75aadd26f04c72f71327eb759
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-08 15:16:18 +0300

    ex(is6a): use floats

--- is6a/is.cc
+++ is6a/is.cc
@@ -12,7 +12,7 @@ struct Result {
     float inner[3];
 };
 
-typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
+typedef float float8_t __attribute__((vector_size(8 * sizeof(float))));
 
 /*
 This is the function you need to implement. Quick reference:
@@ -25,39 +25,35 @@ Result segment(int ny, int nx, const float *data) {
     int nxp = nx + 1;
     int nyp = ny + 1;
 
-    vector<double4_t> pref_s(nyp * nxp, double4_t{0, 0, 0, 0});
-    vector<double4_t> pref_ss(nyp * nxp, double4_t{0, 0, 0, 0});
+    vector<float> pref_s(nyp * nxp, 0);
+    vector<float> pref_ss(nyp * nxp, 0);
 
     for (int y = 0; y < ny; y++) {
         for (int x = 0; x < nx; x++) {
-            for (int c = 0; c < 3; c++) {
-                pref_s[(x + 1) + nxp * (y + 1)][c] =
-                    data[c + 3 * x + 3 * nx * y] +
-                    pref_s[(x + 1) + nxp * y][c] +
-                    pref_s[x + nxp * (y + 1)][c] - pref_s[x + nxp * y][c];
-                pref_ss[(x + 1) + nxp * (y + 1)][c] =
-                    (data[c + 3 * x + 3 * nx * y] *
-                     data[c + 3 * x + 3 * nx * y]) +
-                    pref_ss[(x + 1) + nxp * y][c] +
-                    pref_ss[x + nxp * (y + 1)][c] - pref_ss[x + nxp * y][c];
-            }
+            pref_s[(x + 1) + nxp * (y + 1)] =
+                data[3 * x + 3 * nx * y] + pref_s[(x + 1) + nxp * y] +
+                pref_s[x + nxp * (y + 1)] - pref_s[x + nxp * y];
+            pref_ss[(x + 1) + nxp * (y + 1)] =
+                (data[3 * x + 3 * nx * y] * data[3 * x + 3 * nx * y]) +
+                pref_ss[(x + 1) + nxp * y] + pref_ss[x + nxp * (y + 1)] -
+                pref_ss[x + nxp * y];
         }
     }
 
     Result global_result = Result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
     double global_min_tsse = 1e9;
 
-    double4_t total_s = pref_s[nx + nxp * ny];
-    double4_t total_ss = pref_ss[nx + nxp * ny];
+    float total_s = pref_s[nx + nxp * ny];
+    float total_ss = pref_ss[nx + nxp * ny];
 
 #pragma omp parallel for schedule(dynamic, 1)
     for (int y0 = 0; y0 < ny; y0++) {
-        double min_tsse = 1e9;
+        float min_tsse = 1e9;
         Result result = Result{0, 0, 0, 0, {0, 0, 0}, {0, 0, 0}};
-        vector<double4_t> diff_s(nxp);
-        vector<double4_t> diff_ss(nxp);
-        vector<double> inv_in(nxp);
-        vector<double> inv_out(nxp);
+        vector<float> diff_s(nxp);
+        vector<float> diff_ss(nxp);
+        vector<float> inv_in(nxp);
+        vector<float> inv_out(nxp);
         for (int y1 = y0 + 1; y1 <= ny; y1++) {
             int iy0 = nxp * y0;
             int iy1 = nxp * y1;
@@ -65,54 +61,51 @@ Result segment(int ny, int nx, const float *data) {
                 diff_s[x] = pref_s[x + iy1] - pref_s[x + iy0];
                 diff_ss[x] = pref_ss[x + iy1] - pref_ss[x + iy0];
             }
-            double row_n = (double)(y1 - y0);
-            double total_nd = (double)(nx * ny);
+            float row_n = (y1 - y0);
+            float total_nd = (nx * ny);
             for (int k = 1; k <= nx; k++) {
-                double in_n = row_n * k;
-                double out_n = total_nd - in_n;
+                float in_n = row_n * k;
+                float out_n = total_nd - in_n;
                 inv_in[k] = 1.0 / in_n;
                 inv_out[k] = (out_n > 0.0) ? 1.0 / out_n : 0.0;
             }
             for (int x0 = 0; x0 < nx; x0++) {
-                double4_t d_x0 = diff_s[x0];
-                double4_t dd_x0 = diff_ss[x0];
+                float d_x0 = diff_s[x0];
+                float dd_x0 = diff_ss[x0];
                 for (int x1 = x0 + 1; x1 <= nx; x1++) {
                     int k = x1 - x0;
-                    double inv_i = inv_in[k];
-                    double inv_o = inv_out[k];
+                    float inv_i = inv_in[k];
+                    float inv_o = inv_out[k];
                     if (inv_o == 0.0)
                         continue;
 
-                    double4_t inside_sum = diff_s[x1] - d_x0;
-                    double4_t inside_sum_sq = diff_ss[x1] - dd_x0;
+                    float inside_sum = diff_s[x1] - d_x0;
+                    float inside_sum_sq = diff_ss[x1] - dd_x0;
 
-                    double4_t outside_sum = total_s - inside_sum;
-                    double4_t outside_sum_sq = total_ss - inside_sum_sq;
+                    float outside_sum = total_s - inside_sum;
+                    float outside_sum_sq = total_ss - inside_sum_sq;
 
-                    double4_t inside_sse =
+                    float inside_sse =
                         inside_sum_sq - inside_sum * inside_sum * inv_i;
-                    double4_t outside_sse =
+                    float outside_sse =
                         outside_sum_sq - outside_sum * outside_sum * inv_o;
 
-                    double tsse = inside_sse[0] + inside_sse[1] +
-                                  inside_sse[2] + outside_sse[0] +
-                                  outside_sse[1] + outside_sse[2];
+                    float tsse = inside_sse + outside_sse;
 
                     if (tsse > global_min_tsse)
                         continue;
 
                     if (tsse < min_tsse) {
                         min_tsse = tsse;
-                        result = Result{y0,
-                                        x0,
-                                        y1,
-                                        x1,
-                                        {(float)(outside_sum[0] * inv_o),
-                                         (float)(outside_sum[1] * inv_o),
-                                         (float)(outside_sum[2] * inv_o)},
-                                        {(float)(inside_sum[0] * inv_i),
-                                         (float)(inside_sum[1] * inv_i),
-                                         (float)(inside_sum[2] * inv_i)}};
+                        result = Result{
+                            y0,
+                            x0,
+                            y1,
+                            x1,
+                            {(outside_sum * inv_o), (outside_sum * inv_o),
+                             (outside_sum * inv_o)},
+                            {(inside_sum * inv_i), (inside_sum * inv_i),
+                             (inside_sum * inv_i)}};
                     }
                 }
             }
