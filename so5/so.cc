5d7be3bdc9bdd25ea93abcaa0b5f7db92dc9fe98
3nd3r1 <ender.spam.guru@gmail.com>
2026-05-08 11:01:41 +0300

    ex(so5): naive solution

--- so5/so.cc
+++ so5/so.cc
@@ -1,9 +1,40 @@
 #include <algorithm>
+#include <omp.h>
 
 typedef unsigned long long data_t;
 
+int partition(data_t *data, int l, int r) {
+    data_t pivot = data[r];
+    int i = l - 1;
+
+    for (int j = l; j <= r; j++) {
+        if (data[j] < pivot) {
+            i++;
+            std::swap(data[i], data[j]);
+        }
+    }
+
+    std::swap(data[i + 1], data[r]);
+    return i + 1;
+}
+
+void quickSort(data_t *data, int l, int r) {
+    if (r - l < 1000000) {
+        std::sort(data + l, data + r + 1);
+        return;
+    }
+
+    int pi;
+#pragma omp task
+    pi = partition(data, l, r);
+#pragma omp task
+    quickSort(data, l, pi - 1);
+#pragma omp taskwait
+    quickSort(data, pi + 1, r);
+}
+
 void psort(int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of quicksort.
-    std::sort(data, data + n);
+#pragma omp parallel
+#pragma omp single
+    quickSort(data, 0, n - 1);
 }
